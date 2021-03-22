fb7469ceab12cc01a30f1acff957883c628ce249
Edwin Bryan Chalacán Araujo <chalacan4@gmail.com>
2021-03-22 16:54:27 -0500

    test
    test

--- comiteTituloJpa/src/ec/edu/uce/comiteEtica/servicio/interfaz/LocalidadPaisService.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.uce.comiteEtica.servicio.interfaz;
-import java.util.List;
-import ec.edu.uce.comiteEtica.jpa.entidad.LocalidadPais;
-public interface LocalidadPaisService {
-
-	public List<LocalidadPais> listarLocalidadPais();
-	
-}
