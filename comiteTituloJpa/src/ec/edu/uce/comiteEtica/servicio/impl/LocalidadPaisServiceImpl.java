9e26b2b9f0fdcc17311238330e81db94cad9405c
Edwin Bryan Chalacán Araujo <chalacan4@gmail.com>
2021-03-22 16:47:22 -0500

    test
    test

--- comiteTituloJpa/src/ec/edu/uce/comiteEtica/servicio/impl/LocalidadPaisServiceImpl.java
+++ comiteTituloJpa/src/ec/edu/uce/comiteEtica/servicio/impl/LocalidadPaisServiceImpl.java
@@ -1,5 +1,16 @@
 package ec.edu.uce.comiteEtica.servicio.impl;
 
-public class LocalidadPaisServiceImpl {
+import java.util.List;
+
+import ec.edu.uce.comiteEtica.jpa.entidad.LocalidadPais;
+import ec.edu.uce.comiteEtica.servicio.interfaz.LocalidadPaisService;
+
+public class LocalidadPaisServiceImpl implements LocalidadPaisService{
+
+	@Override
+	public List<LocalidadPais> listarLocalidadPais() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
